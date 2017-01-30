a3d98fed0083d726af38dd5f8a534ebde2c321b0
Rhindani Loyalar <rhindani@live.com>
2017-01-30 17:25:38 -0500

    Test one

--- MagiNation/Assets/Scripts/Cards/BaseCard.cs
+++ MagiNation/Assets/Scripts/Cards/BaseCard.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BaseCard : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class BaseCard 
+{
+	private string name;
+	private int currentEnergy;
 }
+
